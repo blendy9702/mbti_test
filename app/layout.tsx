78ee9fa9f2f561b11215bd8b96d16cb5497a3c78
blendy9702 <lch99206@naver.com>
2025-07-26 20:22:12 +0900

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,24 @@
-import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
-import './globals.css'
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
-}
+  title: "MBTI 테스트",
+  description: "MBTI 테스트",
+  icons: {
+    icon: "/icon.png",
+  },
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ko" suppressHydrationWarning>
       <head>
         <style>{`
 html {
@@ -27,5 +30,5 @@ html {
       </head>
       <body>{children}</body>
     </html>
-  )
+  );
 }
